ea144a1764f1cbf157df5ea0018ea979e1b7f387
TRUNG <ductrungkhtn@gmail.com>
2014-02-04 23:22:35 +0100

    add install file

--- _install+.php
+++ _install+.php
@@ -4,4 +4,27 @@
  * User: loveallufev
  * Date: 2/4/14
  * Time: 10:59 PM
- */ 
+ */
+
+if (!file_exists('logs')){
+    mkdir('logs', 0777, true);
+    echo "create directory logs <br/>" ;
+}
+
+if (!file_exists('songs')){
+    mkdir('songs', 0777, true);
+    echo "create directory songs <br/>";
+}
+
+if (!file_exists('temps')){
+    mkdir('temps', 0777, true);
+    echo "create directory temp <br/>";
+}
+
+if (!file_exists('upload')){
+    mkdir('upload', 0777, true);
+    echo "create directory upload <br/>";
+}
+
+chmod('Config/Configuration.xml', 775);
+chmod('Config/LuckyVoice.xml', 775);
