395763c4055e8c1495a745a21fbab287a32b7875
TRUNG <ductrungkhtn@gmail.com>
2014-02-07 14:05:46 +0100

    modify install.php

--- _install+.php
+++ _install+.php
@@ -26,6 +26,70 @@ if (!file_exists('upload')){
     echo "create directory upload <br/>";
 }
 
+if (!file_exists('Config/LuckyVoice.xml')){
+    $file = fopen('Config/LuckyVoice.xml', 'wb');
+    $file->write('<?xml version="1.0" standalone="yes"?>
+<configuration>
+    <login>
+        <sessionid>5zmbjoka55yvuq1ura2pnv2udhmek92s</sessionid>
+        <expires>Wed, 04-Feb-2015 00:34:27 GMT</expires>
+    </login>
+    <api_token>
+        <sessionid>5zmbjoka55yvuq1ura2pnv2udhmek92s</sessionid>
+        <token>c7910d695fc646e39d9908069bfe2877</token>
+        <expires>2014-02-04T06:15:41.464677+00:00</expires>
+    </api_token>
+</configuration>'
+);
+    $file->close();
+}
+
+if (!file_exists('Config/Configuration.xml')){
+    $file = fopen('Config/Configuration.xml', 'wb');
+    $file->write('<?xml version="1.0" standalone="yes"?>
+<configuration>
+    <connection>
+        <host>localhost</host>
+        <username>root</username>
+        <password>root</password>
+        <database>karaoke</database>
+        <port>8889</port>
+    </connection>
+    <mod_rewrite>on</mod_rewrite>
+    <luckyvoice>
+        <email>ductrungkhtn@gmail.com</email>
+        <password>ductrunglk89</password>
+    </luckyvoice>
+    <modules>
+        <assistants>
+            <luckyvoice>
+                <config>
+                    <email>ductrungkhtn@gmail.com</email>
+                    <password>ductrunglk89</password>
+                </config>
+                <prefix>L</prefix>
+                <language>en</language>
+                <class>Model_LuckyVoiceAssistant</class>
+                <active>1</active>
+            </luckyvoice>
+            <zing>
+                <language>vi</language>
+                <class>Model_ZingAssistant</class>
+                <active>1</active>
+                <prefix>Z</prefix>
+            </zing>
+        </assistants>
+        <default>luckyvoice</default>
+    </modules>
+    <cache>
+        <lastsongid>-1</lastsongid>
+    </cache>
+</configuration>');
+    $file->close();
+}
+
+
+
 $testGD = get_extension_funcs("gd"); // Grab function list
 if (!$testGD){ echo "GD not even installed."; exit; }
 echo"<pre>".print_r($testGD,true)."</pre>";
