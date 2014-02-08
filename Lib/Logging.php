adfe62d498de83fd3557029259bfa317f68936ff
TRUNG <ductrungkhtn@gmail.com>
2014-02-08 14:41:54 +0100

    nothing

--- Lib/Logging.php
+++ Lib/Logging.php
@@ -43,12 +43,12 @@ class Lib_Logging {
         }
         // set default log file for Linux and other systems
         else {
-            $log_file_default = '/tmp/logfile.txt';
+            $log_file_default = 'logs/logfile.txt';
         }
         // define log file from lfile method or use previously set default
         $lfile = $this->log_file ? $this->log_file : $log_file_default;
         // open log file for writing only and place file pointer at the end of the file
         // (if the file does not exist, try to create it)
-        $this->fp = fopen($lfile, 'a') or exit("Can't open $lfile!");
+        $this->fp = fopen($lfile, 'a+') or exit("Can't open $lfile!");
     }
 }
