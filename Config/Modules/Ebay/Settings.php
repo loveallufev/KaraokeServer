e70ee4d6fec4962ea4b9ba7349c604231a78fb0d
loveallufev <ductrungkhtn@gmail.com>
2014-04-22 11:39:47 +0200

    Update Settings.php

--- Config/Modules/Ebay/Settings.php
+++ Config/Modules/Ebay/Settings.php
@@ -5,7 +5,7 @@
  * Date:  5/21/13
  * Group: Hieu-Trung
 */
-define('AWS_API_KEY', 'AKIAJFPRH7S6FI6JM32Q');
-define('AWS_API_SECRET_KEY', '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');
+define('AWS_API_KEY', '');
+define('AWS_API_SECRET_KEY', '');
 define('AWS_ASSOCIATE_TAG', 'ASSOCIATE TAG');
 define('AWS_ANOTHER_ASSOCIATE_TAG', 'ANOTHER ASSOCIATE TAG');
